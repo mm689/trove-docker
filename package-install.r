c60d38fddadb8df2977a264c27de7723c33df174
Martin McCaffery <mccaffery.martin@gmail.com>
2020-04-09 14:27:34 +0100

    Added svglite R package for graphing

--- package-install.r
+++ package-install.r
@@ -5,5 +5,6 @@ source("package-list.r")
 install.packages(list.of.packages)
 
 # Assert that install succeeded.
+cat("\nAll packages installed. Loading them for verification...\n")
 for (p in list.of.packages)
 	library(p, character.only=T)
--- package-list.r
+++ package-list.r
@@ -1,4 +1,4 @@
 list.of.packages <- c("RMySQL", "jsonlite", "methods", "ggplot2", "gtools",
 	"stringr", "NLP","RColorBrewer","tm","wordcloud", 'stringdist', 'plotrix',
 	'tidytext','tidyverse', 'ggpubr', 'data.table', 'testthat', 'mockery',
-	'plumber', "httr")
+	'plumber', 'httr', 'svglite')
