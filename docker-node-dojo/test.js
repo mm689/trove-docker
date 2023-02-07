33c414a9e81366b9f21373a32d0f907d51aba0b0
Martin McCaffery <martin.mccaffery@thoughtworks.com>
2023-02-07 20:37:39 +0000

    Added graphviz to node images

--- docker-node-dojo/test.js
+++ docker-node-dojo/test.js
@@ -2,3 +2,15 @@
 const ansiRegex = require('ansi-regex');
 var assert = require('assert');
 assert(ansiRegex().test('\u001B[4mcake\u001B[0m'));
+
+const graphviz = require('graphviz')
+const graph = graphviz.digraph()
+graph.addNode('node1')
+
+const confirmSvg = (data) => {
+    const svg = data.toString()
+    if (!svg.includes('<g id="node1" class="node">')) {
+        throw Error('SVG does not appear to be valid')
+    }
+}
+graph.render({ use: 'dot', type: 'svg' }, confirmSvg)
