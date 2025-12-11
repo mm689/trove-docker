a1621ed0c72e8cdaf99e47b032bd073e101ce509
CircleCI workflow <trove-docker@app.circleci.com>
2025-12-11 09:43:48 +0000

    [Automatic] Update R package list

--- package-list.lambda.r
+++ package-list.lambda.r
@@ -3,17 +3,18 @@
 list.of.packages <- c(
     "base64enc", # returning binary data from Lambda to API Gateway
     "aws.signature", # retrieving database connection params
-    "DBI",       # some query-related interpolation functions
-    "jsonlite",  # encoding and decoding JSON
-    "ggplot2",   # graphs
-    "logger",    # lambda logging
-    "httr",      # lambda polling for work and returning responses
-    "plotrix",   # wordcloud legends
-    "RMariaDB",  # database access
-    "stringr",   # various text-processing helpers
-    "svglite",   # returning graphs as SVG
-    "tibble",    # for words processing
-    "tidytext",  # stopwords for word wordclouds
-    "dplyr",     # arrange, rename, etc.
-    "wordcloud"  # wordcloud generation
+    "DBI", # some query-related interpolation functions
+    "jsonlite", # encoding and decoding JSON
+    "ggplot2", # graphs
+    "logger", # lambda logging
+    "hms", # processing entry end times / metadata
+    "httr", # lambda polling for work and returning responses
+    "plotrix", # wordcloud legends
+    "RMariaDB", # database access
+    "stringr", # various text-processing helpers
+    "svglite", # returning graphs as SVG
+    "tibble", # for words processing
+    "tidytext", # stopwords for word wordclouds
+    "dplyr", # arrange, rename, etc.
+    "wordcloud" # wordcloud generation
 )
